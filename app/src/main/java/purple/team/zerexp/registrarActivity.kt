458af683b8fa01dddd85cd699ad06b5871718619
Isai Perez <72577853+alfredo2201@users.noreply.github.com>
2022-03-29 20:10:46 -0700

    Proyecto Zerexp Apps Moviles

--- app/src/main/java/purple/team/zerexp/registrarActivity.kt
+++ app/src/main/java/purple/team/zerexp/registrarActivity.kt
@@ -1,11 +1,11 @@
 package purple.team.zerexp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class registrarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registrar)
     }
 }
--- app/src/test/java/purple/team/zerexp/ExampleUnitTest.kt
+++ app/src/test/java/purple/team/zerexp/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package purple.team.zerexp
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
