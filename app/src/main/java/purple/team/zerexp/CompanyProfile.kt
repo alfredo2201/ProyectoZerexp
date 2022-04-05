e377e6b8e91c6bdfb4935ee45d75671c5862d134
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2022-04-05 00:33:23 -0700

    5 pantallas agregadas. Pantallas agregadas:  - Perfil de usuario  - Perfil de empresa  - Feed de inicio ( - SIN TERMINAR - )  - Mensajes (seccion de chats) ( - FALTA EL BOTTOM NAV BAR - )  - Side bar ( - SIGUE SIN COMPROBARSE QUE REALMENTE SE MUESTRE - )

---
+++ app/src/main/java/purple/team/zerexp/CompanyProfile.kt
@@ -0,0 +1,11 @@
+package purple.team.zerexp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompanyProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company_profile)
+    }
+}
---
+++ app/src/main/java/purple/team/zerexp/SideBarMenu.kt
@@ -0,0 +1,11 @@
+package purple.team.zerexp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SideBarMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_side_bar_menu)
+    }
+}
