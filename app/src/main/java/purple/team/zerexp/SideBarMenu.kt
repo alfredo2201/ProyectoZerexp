007c87702ca77b13b3afca9164abaaf25366ca46
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2022-04-05 11:14:57 -0700

    Cambios en pantallas Cambios en las siguientes pantallas:  - Mensajes (seccion de chats) ( BOTTOM NAV BAR AGREGADO ) ( - FALTA CHECAR SI DE VERDAD FUNCIONA - )  - Side bar ( ICONOS AGREGADOS ) ( - SIGUE SIN COMPROBARSE QUE REALMENTE SE MUESTRE - )

--- app/src/main/java/purple/team/zerexp/SideBarMenu.kt
+++ app/src/main/java/purple/team/zerexp/SideBarMenu.kt
@@ -2,10 +2,42 @@ package purple.team.zerexp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_side_bar_menu.*
 
 class SideBarMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_side_bar_menu)
+
+        toolbar("Menu Lateral")
+
     }
+
+    fun toolbar(title: String) {
+        setSupportActionBar(tb_side_bar)
+
+        var ab: ActionBar? = supportActionBar
+
+        if (ab != null) {
+            ab.setHomeAsUpIndicator(R.drawable.ic_action_name_gray)
+            ab.setDisplayHomeAsUpEnabled(true)
+            ab.title = title
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> {
+                dwl_side_bar.openDrawer(GravityCompat.START)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
