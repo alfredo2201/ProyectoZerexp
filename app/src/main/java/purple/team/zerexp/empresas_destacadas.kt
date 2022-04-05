f5c2947803eac89e856dbc970d619330d40ccf6a
Julian Javier Pimeinta Rendon <jpimientarm56@gmail.com>
2022-04-05 11:28:56 -0700

    Agrege actividad de favoritos y empresas destacadas

---
+++ app/src/main/java/purple/team/zerexp/empresas_destacadas.kt
@@ -0,0 +1,11 @@
+package purple.team.zerexp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class empresas_destacadas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empresas_destacadas)
+    }
+}
---
+++ app/src/main/java/purple/team/zerexp/favoritos.kt
@@ -0,0 +1,11 @@
+package purple.team.zerexp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class favoritos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favoritos)
+    }
+}
