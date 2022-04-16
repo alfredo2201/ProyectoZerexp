37b926402fb295610eb59b2c5707d4d01d36d54d
Isai Perez <72577853+alfredo2201@users.noreply.github.com>
2022-04-16 15:04:34 -0700

    Primera parte de la aplicacion con diseño

--- app/src/main/java/purple/team/zerexp/MainActivity.kt
+++ app/src/main/java/purple/team/zerexp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
