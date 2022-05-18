dc5dcb49f500fee3e36ab99dc5781829a56b4fc2
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2022-05-18 09:38:18 -0700

    muestra hora y el ultimo mensaje enviado en la lista de chats

--- app/src/main/java/purple/team/zerexp/adaptadores/ChatAdapter.kt
+++ app/src/main/java/purple/team/zerexp/adaptadores/ChatAdapter.kt
@@ -2,23 +2,28 @@ package purple.team.zerexp.adaptadores
 
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import androidx.appcompat.app.AppCompatActivity
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.message_view.view.*
 import purple.team.zerexp.Mensajes
 import purple.team.zerexp.R
 import purple.team.zerexp.modelos.Chat
+import purple.team.zerexp.modelos.Message
+import java.time.ZoneId
 
 class ChatAdapter: BaseAdapter {
 
     var chats = ArrayList<Chat>()
     var context: Context? = null
+    val db = Firebase.firestore
 
     constructor(context: Context, chats: ArrayList<Chat>) {
         this.context = context
@@ -37,13 +42,22 @@ class ChatAdapter: BaseAdapter {
         return p0.toLong()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
         var chat = chats[p0]
         var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         var view = inflator.inflate(R.layout.message_view, null)
+        val chatRef = db.collection("chats").document(chat.id.trim())
         view.tv_username.setText(chat.name)
-        view.tv_last_message.setText(chat.id)
-        view.tv_last_message_time.setText("13:07")
+        chatRef.collection("messages").orderBy("dob", Query.Direction.ASCENDING)
+            .get().addOnSuccessListener { messagesDB ->
+                if (messagesDB.size() > 0) {
+                    val messages = messagesDB.toObjects(Message::class.java) as ArrayList<Message>
+                    view.tv_last_message.setText(messages.last().message)
+                    val time = messages.last().dob.toInstant().atZone(ZoneId.systemDefault()).toLocalTime()
+                    view.tv_last_message_time.setText("${time.hour}:${time.minute}")
+                }
+            }
         view.ll_chat_info_layout.setOnClickListener {
             var intent = Intent(context, Mensajes::class.java)
             intent.putExtra("chatId", chat.id)
